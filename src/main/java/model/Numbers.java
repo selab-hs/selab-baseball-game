7d3c02f6a5ccb1344906e22bfa2ef6e0717c1e71
LSH-3016 <a673813@naver.com>
2022-10-08 02:51:24 +0900

    매직넘버 클래스

---
+++ src/main/java/model/Numbers.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Numbers {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int THREE = 3;
+    public static final int TEN = 10;
+    public static final int HUNDRED = 100;
+    public static final int MAX_NUMBER = 999;
+}
