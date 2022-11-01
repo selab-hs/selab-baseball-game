61e378f81d08a993af77334dd601d6024b909e19
LSH-3016 <a673813@naver.com>
2022-11-02 03:42:50 +0900

    수정 : 상수처리에 대한 수정

--- src/main/java/model/Numbers.java
+++ src/main/java/model/Numbers.java
@@ -2,9 +2,6 @@ package model;
 
 public class Numbers {
     public static final int ZERO = 0;
-    public static final int ONE = 1;
-    public static final int THREE = 3;
-    public static final int TEN = 10;
-    public static final int HUNDRED = 100;
+    public static final int MIN_NUMBER = 102;
     public static final int MAX_NUMBER = 999;
 }
