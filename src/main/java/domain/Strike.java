275ec7a06b4fe9adb46e9691fd983b1762f46da1
LSH-3016 <a673813@naver.com>
2022-11-02 03:44:37 +0900

    수정 : for문 안의 if문 수정

--- src/main/java/domain/Strike.java
+++ src/main/java/domain/Strike.java
@@ -6,18 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Strike {
+    private int strike;
+
     public int checkingStrike(List<Integer> randomList, List<Integer> userList) {
-        int strike = Numbers.ZERO;
+        strike = Numbers.ZERO;
         List<Integer> strikeIntegers = new ArrayList<>();
         randomList.forEach(number -> strikeIntegers.add(userList.indexOf(number)));
         for (int index : strikeIntegers) {
             if (index < Numbers.ZERO) {
                 continue;
             }
-            if (randomList.get(index).equals(userList.get(index))) {
-                strike++;
-            }
+            compareNumber(randomList.get(index), strikeIntegers.get(index));
         }
         return strike;
+    }
+
+    private void compareNumber(int number1, int number2) {
+        if (number1 == number2) {
+            strike++;
+        }
     }
 }
