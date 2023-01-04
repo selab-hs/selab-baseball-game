d588c70fef9f797853e15bd162198eeb2b785335
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-01-04 11:36:30 +0900

    변수로 객체 상태 제어

--- src/main/java/controller/BaseballGame.java
+++ src/main/java/controller/BaseballGame.java
@@ -2,27 +2,29 @@ package controller;
 
 import service.Computer;
 import model.Judgement;
+import service.Input;
 import service.Referee;
 import view.ResultWindow;
 
 import java.util.Scanner;
 
 public class BaseballGame {
+    private boolean isGameGoingOn = true;
+
     public void startGame() {
         String computerNumber = new Computer().getNonDuplicateNumber();
-        boolean playerWin = false;
 
-        while (! playerWin){
-            playerWin = isPlayerWin(computerNumber);
+        while (isGameGoingOn) {
+            isPlayerWin(computerNumber);
         }
     }
 
-    private boolean isPlayerWin(String computerNumber) {
+    private void isPlayerWin(String computerNumber) {
         String playerNumber = new Scanner(System.in).nextLine();
 
         Judgement judgement = new Referee().notifyTheResult(playerNumber, computerNumber);
 
         new ResultWindow().showResult(judgement);
-        return judgement.getStrike() == 3;
+        this.isGameGoingOn = !(judgement.getStrike() == 3);
     }
 }
