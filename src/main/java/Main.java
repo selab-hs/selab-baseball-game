e0c14ff4a82006825b6799deb0a83adecbd947a7
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-01-07 20:20:55 +0900

    메소드명 변경에 따른 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-import controller.BaseballGame;
+import controller.BaseballGameController;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Play Baseball Game");
 
-        new BaseballGame().startGame();
+        new BaseballGameController().startGame();
     }
 }
