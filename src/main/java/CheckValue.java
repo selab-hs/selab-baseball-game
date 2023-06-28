11bd569d94bd5055f888366d927cca34aaaf2a16
ds4pae <ds4pae@gmail.com>
2023-06-28 18:53:58 +0900

    update : checkString

--- src/main/java/CheckValue.java
+++ src/main/java/CheckValue.java
@@ -1,13 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class CheckValue {
 
-    public CheckValue(String input) {
-
-
-        System.out.println(input);
+    public CheckValue(String inputValue, String randomValue) {
+        checkString(inputValue);
+        checkValue(inputValue,randomValue);
+        System.out.println(inputValue + " " + randomValue);
     }
+
+    private void checkValue(String inputValue, String randomValue) {
+        //야구게임
+    }
+
+    private boolean checkString(String value) {
+        try {
+            if (!Pattern.matches("\\d{3}", value))
+                throw new NumberFormatException("입력된 문자열은 3자리 숫자가 아닙니다.");
+        } catch (NumberFormatException e) {
+            System.err.println("NumberFormatException : " + e.getMessage());
+            System.exit(0);
+        }
+        return false;
+    }
+
 
 
 }
