acaee8367df45e5df2e152d520fef8091ba438c4
ds4pae <ds4pae@gmail.com>
2023-06-29 20:14:42 +0900

    delete CheckValue.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Play Baseball Game");
-        RandomValue randomValue = new RandomValue();
-        GetInputValue getInputValue = new GetInputValue();
-
-        CheckValue checkValue = new CheckValue(getInputValue.getInputValue(),RandomValue.getRandomValue());
+        PlayBaseballGame playBaseballGame = new PlayBaseballGame();
     }
 }
