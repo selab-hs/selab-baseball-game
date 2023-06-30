bd244f0e77d1ccda0252ac76bf893bd3a1e5841e
ds4pae <ds4pae@gmail.com>
2023-06-30 20:21:59 +0900

    Ball.java & Strike.java 생성

---
+++ src/main/java/Ball.java
@@ -0,0 +1,11 @@
+public class Ball {
+    private final int ball;
+
+    public Ball(int ball) {
+        this.ball = ball;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
---
+++ src/main/java/Strike.java
@@ -0,0 +1,11 @@
+public class Strike {
+    private final int strike;
+
+    public Strike(int strike) {
+        this.strike = strike;
+    }
+
+    public int getStrike(){
+        return strike;
+    }
+}
