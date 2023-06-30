2f5114c93e5cafb286ec3976ae9e0dd8fbb92f92
ds4pae <ds4pae@gmail.com>
2023-06-30 20:22:48 +0900

    출력 조건 변경

--- src/main/java/PrintResult.java
+++ src/main/java/PrintResult.java
@@ -1,12 +1,10 @@
 public class PrintResult {
-    public PrintResult(int strike, int ball) {
-        System.out.println(strike + " Strike, " + ball + " Ball");
+    public PrintResult(int strike, int ball,String randomValue) {
+        if(strike == 3){
+            System.out.println("BASEBALL WIN~");
+            System.out.println("RandomValue : " + randomValue);
+            System.exit(1);
+        }else
+            System.out.println(strike + " Strike, " + ball + " Ball");
     }
-
-    public PrintResult() {
-        System.out.println("BASEBALL WIN~");
-        System.out.println("RandomValue : " + RandomValue.getRandomValue());
-    }
-
-
 }
