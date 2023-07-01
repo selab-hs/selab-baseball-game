8f9095905eb44857319846feeb0d35b0bedffd1f
ds4pae <ds4pae@gmail.com>
2023-07-01 19:31:35 +0900

    add : checkBall 메소드

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,8 +1,19 @@
 public class Ball {
-    private final int ball;
+    private int ball;
 
+    public Ball(){
+
+    }
     public Ball(int ball) {
         this.ball = ball;
+    }
+
+    public boolean checkBall(char inputChar, String randomValue, int count) {
+        if (randomValue.contains(String.valueOf(inputChar))) {
+            ball++;
+            return true;
+        }
+        return false;
     }
 
     public int getBall() {
