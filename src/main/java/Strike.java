1aa7fb633cc356800cfe66228dd4a8dc0c43ed93
ds4pae <ds4pae@gmail.com>
2023-07-01 19:31:47 +0900

    add : checkStrike 메소드

--- src/main/java/Strike.java
+++ src/main/java/Strike.java
@@ -1,8 +1,17 @@
 public class Strike {
-    private final int strike;
+    private int strike;
+    public Strike(){
 
+    }
     public Strike(int strike) {
         this.strike = strike;
+    }
+    public boolean checkStrike(char inputChar, String randomValue, int count){
+        if(inputChar == randomValue.charAt(count)){
+            strike++;
+            return true;
+        }
+        return false;
     }
 
     public int getStrike(){
